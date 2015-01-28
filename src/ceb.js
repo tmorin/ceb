ac45c2c043c24b7f38432e1bde92f0ce41293705
tmorin <tcmorin@gmail.com>
2015-01-28 19:14:23 +0100

    add comments

--- src/ceb.js
+++ src/ceb.js
@@ -1,6 +1,8 @@
 (function (g, factory) {
     /* globals module:0, define:0 */
 
+    // Export the **ceb** function according the detected loader.
+
     'use strict';
 
     /* istanbul ignore next */
@@ -16,79 +18,106 @@
 
     'use strict';
 
-    /* TOOLS */
+    // ## Tools
 
+    // Return a new empty function.
     function emptyFn() {
         return function () {};
     }
 
+    // List the values of an object.
     function listValues(o) {
         return Object.getOwnPropertyNames(o).map(function (propName) {
             return o[propName];
         });
     }
 
-    // transform property notation to attribute notation
+    // Transform property notation to attribute notation.
     function fromCamelCaseToHyphenCase(value) {
         return value.split(/(?=[A-Z])/).map(function (part) {
             return part.charAt(0).toLowerCase() + part.slice(1);
         }).join('-');
     }
 
+    // Compare object according to their levels.
     function compareLevels(a, b) {
         return a.level - b.level;
     }
 
+    // Apply an attribute value to an element.
     function applyAttributeValue(el, attName, value, isBoolean) {
         if (isBoolean) {
+            // Handle boolean value
             if (value && !el.hasAttribute(attName)) {
+                // Set attribute only if the attribute is not preset
                 el.setAttribute(attName, '');
             } else if (!value && el.hasAttribute(attName)) {
+                // The value is false so the attribute must be removed
                 el.removeAttribute(attName);
             }
         } else {
+            // Handle none boolean value
             if ((value === null || value === undefined) && el.hasAttribute(attName)) {
+                // There is no value, so the attribute must be removed
                 el.removeAttribute(attName);
             } else if ((value !== null && value !== undefined) && el.getAttribute(attName) !== value) {
+                // Sync the attribute value with value
                 el.setAttribute(attName, value);
             }
         }
     }
 
+    // Create an accessor function in order to wrap the original accessor with its accessors.
     function accessorFactory(wrappers, wrapped) {
+        // Order the stack of wrappers
         var stack = wrappers.sort(compareLevels);
         return function accessor() {
             var el = this;
+            // The first function of the stack is called,
+            // calling the next function when its argument function next is called.
+            // The original accessor is the last call.
             return stack.reduce(function (previous, current) {
                 return current.bind(el, previous, el);
             }, wrapped.bind(el, el)).apply(el, arguments);
         };
     }
 
+    // Create the accessor set for a property linked to an attribute
     function attributeAccessorSetFactory(attName, setter, isBoolean) {
         return function attributeAccessorSet(el, value) {
+            // By default, the attribute value is the set value.
             var attValue = value;
             if (setter) {
+                // The default value can be overridden by a given setter.
                 attValue = setter.call(el, el, value);
             }
+            // Finally apply the attribute to the linked attribute.
             applyAttributeValue(el, attName, attValue, isBoolean);
         };
     }
 
+    // Create the accessor get for a property linked to an attribute
     function attributeAccessorGetFactory(attName, getter, isBoolean) {
         return function attributeAccessorGet(el) {
+            // By default, the returned value is the attribute value.
             var value = isBoolean ? el.hasAttribute(attName) : el.getAttribute(attName);
             if (getter) {
+                // The returned value can be overridden by a given getter.
                 value = getter.call(el, el, value);
             }
+            // Finally the value is returned
             return value;
         };
     }
 
+    // Create an element's method
     function methodFactory(wrappers, wrapped) {
+        // Order the stack of wrappers
         var stack = wrappers.sort(compareLevels);
         return function () {
             var el = this;
+            // The first function of the stack is called, calling the next function when its argument function next is called.
+            // The original method is the last call.
             return stack.reduce(function (previous, current) {
                 return current.bind(el, function next(args) {
                     return previous.apply(el, Array.prototype.slice.call(args).slice(2, args.length));
@@ -97,8 +126,9 @@
         };
     }
 
-    /* BUILD LIFE CYCLE */
+    // ## Life cycle
 
+    // Sanitize a structure to avoid the errors <code>?? not defined</code>
     function sanitizeStructure(struct) {
         struct.prototype = struct.prototype || Object.create(HTMLElement.prototype);
         struct.features = struct.features || [];
@@ -115,11 +145,14 @@
         return struct;
     }
 
+    // Call the setup's method of features.
     function setupFeatures(struct) {
+        // Create a builder from the current structure.
         var b = builder(struct.tagName, {
             struct: struct,
             registered: true
         });
+        // Call setup functions according their features's levels.
         struct.features.sort(compareLevels).forEach(function (feature) {
             if (feature.fn && feature.fn.setup) {
                 feature.fn.setup(struct, b, feature.options);
@@ -127,6 +160,8 @@
         });
     }
 
+    // Create an hash of attributes from the structure's properties.
+    // The hash of attributes is used to keep in sync properties' values and attributes' values.
     function createAttributesHash(struct) {
         return listValues(struct.properties).filter(function (property) {
             return property.attName;
@@ -136,28 +171,31 @@
         }, {});
     }
 
+    // Create an hash of defined properties from the structure's properties.
+    // The defined properties are the input of Object.defineProperties().
     function createDefinedPropertiesHash(struct) {
         return listValues(struct.properties).map(function (property) {
-            // default parameters
+            // By default properties should not be configurable but enumerable.
             var definedProperty = {
                 configurable: false,
                 enumerable: true
             };
 
             if (property.attribute) {
-                // handle properties linked to an attribute
+                // Create attribute accessors.
                 property.set = attributeAccessorSetFactory(property.attName, property.setter, !!property.attribute.boolean);
                 property.get = attributeAccessorGetFactory(property.attName, property.getter, !!property.attribute.boolean);
             } else if (property.hasOwnProperty('value') && property.hasOwnProperty('writable') && !property.writable) {
-                // handle constants
+                // A constant has a value which is not writable.
                 definedProperty.value = property.value;
                 definedProperty.writable = false;
             } else if (!property.set && !property.get) {
+                // A none constant property without accessor must be writable.
                 definedProperty.writable = true;
             }
 
             if (!definedProperty.hasOwnProperty('writable')) {
-                // handle setter and getter
+                // Create the property's function set and get according to their interceptors.
                 var interceptors = struct.interceptors[property.propName] || {};
                 if (property.set) {
                     var setStack = interceptors.set || [];
@@ -169,6 +207,7 @@
                 }
             }
 
+            // Finally add the defined property to the current property
             return Object.assign(property, {
                 property: definedProperty
             });
@@ -178,7 +217,9 @@
         }, {});
     }
 
+    // Create an hash of methods from the structure's methods.
     function createMethodsHash(struct) {
+        // Iterate over methods in order to wrap them with their wrappers.
         return Object.getOwnPropertyNames(struct.methods).map(function (methName) {
             var stack = struct.wrappers[methName] || [];
             var fn = struct.methods[methName];
@@ -192,42 +233,68 @@
         }, {});
     }
 
-    /* BUILD */
+    // ## Build
 
-    var build = function build(struct) {
+    // Work with the structure in order to register the corresponding custom element.
+    function build(struct) {
+        // Clean the structure to avoid potential errors.
         sanitizeStructure(struct);
+
+        // Let's feature enhanced the structure.
         setupFeatures(struct);
 
-        struct.definedProperties = createDefinedPropertiesHash(struct);
+        // Build an hash of attribute for sync attributes' and properties' values.
         struct.attributes = createAttributesHash(struct);
-        struct.methods = createMethodsHash(struct);
 
-        Object.defineProperties(struct.prototype, struct.definedProperties);
-        Object.assign(struct.prototype, struct.methods);
+        // Create the public properties definition of the custom element.
+        var definedProperties = createDefinedPropertiesHash(struct);
+        // Apply the public properties definition to the custom element prototype.
+        Object.defineProperties(struct.prototype, definedProperties);
 
+        // Override the originals methods to create the wrapped ones.
+        var wrappedMethods = createMethodsHash(struct);
+        // Apply the wrapped methods to the custom element prototype.
+        Object.assign(struct.prototype, wrappedMethods);
+
+        // Register the custom element according to its structure.
         return document.registerElement(struct.tagName, struct);
-    };
+    }
 
-    /* Built-in feature */
+    // ## Built-in feature
 
+    // **ceb** defines an internal feature in order to add runtime functionalities:
+    // - initialize properties' values
+    // - add and remove event listeners
+    // - sync properties' values when theirs linked attributes change
+    // - handle delegation of property access to a child element
+
+    // ### Interceptors
+
+    // Intercept write access of delegable properties.
     function delegableSetAccessorInterceptor(property, next, el, value) {
         next(value);
+        // Logic should be done after the effective write.
         var target = el.querySelector(property.delegate.target);
         /* istanbul ignore else  */
         if (target) {
+            // Resolve the eventual targeted property's name or attribute's name.
             var targetPropName = property.delegate.property;
             var targetAttName = property.delegate.attribute;
             if (!targetPropName && !targetAttName) {
                 targetPropName = property.propName;
                 targetAttName = property.attName;
             }
+            // Check the boolean nature of the value.
             var isBoolean = property.attribute && !!property.attribute.boolean;
             if (property.delegate.hasOwnProperty('boolean')) {
                 isBoolean = property.delegate.boolean;
             }
+            // Apply change to the child.
             if (targetAttName) {
+                // Update the child's attribute value.
                 applyAttributeValue(target, targetAttName, value, isBoolean);
             } else {
+                // Update the child's property value.
                 target[targetPropName] = value;
             }
         }
@@ -235,51 +302,67 @@
 
     function delegableGetAccessorInterceptor(property, next, el, value) {
         var result = next(value);
+        // Logic should be done after the effective write.
         var target = el.querySelector(property.delegate.target);
         /* istanbul ignore else  */
         if (target) {
+            // Resolve the eventual targeted property's name or attribute's name.
             var targetPropName = property.delegate.property;
             var targetAttName = property.delegate.attribute;
             if (!targetPropName && !targetAttName) {
                 targetPropName = property.propName;
                 targetAttName = property.attName;
             }
+            // Check the boolean nature of the value.
             var isBoolean = property.attribute && !!property.attribute.boolean;
             if (property.delegate.hasOwnProperty('boolean')) {
                 isBoolean = property.delegate.boolean;
             }
+            // Get value from the child.
             if (targetAttName) {
+                // Get the child's attribute value.
                 result = isBoolean ? target.hasAttribute(targetAttName) : target.getAttribute(targetAttName);
             } else {
+                // Get the child's property value.
                 result = target[targetPropName];
             }
         }
         return result;
     }
 
+    // ### Listeners
+
+    // DOM event listener calling the structure's event listener.
     function eventListener(el, fn, evt) {
         fn(el, evt);
     }
 
+    // ### Feature's functions
+
+    // Initialize the builtInFeature;
     var builtInFeature = emptyFn();
+    // The setup function of the built-in feature
     builtInFeature.setup = function (struct, builder) {
-        // keep only properties configured for delegation
+        // Iterate over structure's properties in order to detect delegable properties.
         listValues(struct.properties).filter(function (property) {
             return property.delegate;
         }).forEach(function (property) {
-            // intercept the setter
             if (!property.attName) {
+                // Force the existence of getter and setter to handle interception.
                 property.set = property.set || emptyFn();
                 property.get = property.get || emptyFn();
             }
+            // Create interceptor for the delegable property.
             builder.intercept(
                 property.propName,
                 delegableSetAccessorInterceptor.bind(this, property),
                 delegableGetAccessorInterceptor.bind(this, property)
             );
         });
+        // Wrap the method createdCallback.
         builder.wrap('createdCallback', function (next, el) {
-            // initialize properties values
+            next(arguments);
+            // Initialize the properties' value after the call of the createdCallback method.
             listValues(struct.properties).forEach(function (property) {
                 if (property.attName) {
                     if (el.hasAttribute(property.attName)) {
@@ -291,11 +374,11 @@
                     el[property.propName] = property.value;
                 }
             });
-            next(arguments);
         });
+        // Wrap the method attachedCallback.
         builder.wrap('attachedCallback', function (next, el) {
             next(arguments);
-            // listen events
+            // Add the event listeners after the call of the attachedCallback method.
             el.__eventHandlers = struct.listeners.map(function (listener) {
                 var target = listener.target ? el.querySelector(listener.target) : el;
                 var callback = eventListener.bind(el, el, listener.fn);
@@ -307,16 +390,18 @@
                 };
             });
         });
+        // Wrap the method detachedCallback.
         builder.wrap('detachedCallback', function (next, el) {
-            // release event handlers
+            // Remove the event listeners before the call of the detachedCallback method.
             el.__eventHandlers.forEach(function (handler) {
                 handler.target.removeEventListener(handler.event, handler.callback, true);
             });
             el.__eventHandlers = null;
             next(arguments);
         });
+        // Wrap the method attributeChangedCallback.
         builder.wrap('attributeChangedCallback', function (next, el, attName, oldVal, newVal) {
-            // sync the attribute value with the property value
+            // Synchronize the attributes' values with their properties
             var property = struct.attributes[attName];
             if (property) {
                 var value = newVal;
