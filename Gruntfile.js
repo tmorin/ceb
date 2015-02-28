5949966fe97bbb8dc6416160dd4e3210c5f3ac6c
tmorin <tcmorin@gmail.com>
2015-02-28 14:01:20 +0100

    prepare release [skip ci]

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        nextVersion: '0.2.5-alpha1',
+        nextVersion: '0.2.5',
 
         watch: {
             js: {
