fc86d77cef320b7ecf9e22ff804161ddc724e1d2
tmorin <tcmorin@gmail.com>
2015-08-07 21:53:35 +0200

    configure saucelab

--- gulp/config.js
+++ gulp/config.js
@@ -25,5 +25,74 @@ module.exports = {
         example: 'example/**/*.js',
         gulp: 'gulp/**/*.js',
         clean: ['./lib', './test', './public']
+    },
+    sauceTasks: {
+        ie: ['slIe11', 'slIe10', 'slIe9'],
+        evergreen: ['slChrome', 'slFirefox'],
+        safari: ['slSafari7', 'slSafari8'],
+        android: ['slAndroid4', 'slAndroid5'],
+    },
+    customLaunchers: {
+        slChrome: {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            version: ''
+        },
+        slFirefox: {
+            base: 'SauceLabs',
+            browserName: 'firefox',
+            version: ''
+        },
+        slIe11: {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            version: '11'
+        },
+        slIe10: {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            version: '10'
+        },
+        slIe9: {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            version: '9'
+        },
+        slAndroid4: {
+            base: 'SauceLabs',
+            browserName: 'android',
+            version: '4.4'
+        },
+        slAndroid5: {
+            base: 'SauceLabs',
+            browserName: 'android',
+            version: '5.0'
+        },
+        slSafari7: {
+            base: 'SauceLabs',
+            browserName: 'safari',
+            version: '7',
+        },
+        slSafari8: {
+            base: 'SauceLabs',
+            browserName: 'safari',
+            version: '8',
+        },
+        slIPhone: {
+            base: 'SauceLabs',
+            browserName: 'iphone',
+            deviceName: 'iPhone',
+            version: '7.1',
+            platform: 'OS X 10.10',
+            'device-orientation': 'portrait'
+        },
+        slIPad: {
+            base: 'SauceLabs',
+            browserName: 'ipad',
+            deviceName: 'iPad',
+            version: '7.1',
+            platform: 'OS X 10.10',
+            'device-orientation': 'portrait'
+        }
     }
 };
--- gulp/tasks/karma.js
+++ gulp/tasks/karma.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 var Server = require('karma').Server;
+
+var config = require('../config');
 
 gulp.task('karma:watch', ['lint'], function (done) {
     new Server({
@@ -17,8 +20,7 @@ gulp.task('karma:watch', ['lint'], function (done) {
 
 gulp.task('karma', ['lint'], function (done) {
     var server = new Server({
-        configFile: __dirname + '/../../karma.conf.js',
-        singleRun: true
+        configFile: __dirname + '/../../karma.conf.js'
     }, function (code, err) {
         if (err) {
             done(err);
@@ -26,4 +28,38 @@ gulp.task('karma', ['lint'], function (done) {
         done();
     });
     server.start();
+});
+
+function createSauceTask(name) {
+    gulp.task('karma:sauce:' + name, ['lint'], function (done) {
+        new Server({
+            configFile: __dirname + '/../../karma.conf.js',
+            sauceLabs: {
+                testName: 'Custom Elements Builder Test',
+                recordVideo: false,
+                recordScreenshots: false
+            },
+            captureTimeout: 3 * 60 * 1000,
+            reporters: ['dots', 'saucelabs', 'coverage'],
+            singleRun: true,
+            autoWatch: false,
+            customLaunchers: config.customLaunchers,
+            browsers: config.sauceTasks[name]
+        }, function (code, err) {
+            if (err) {
+                done(err);
+            }
+            done();
+        }).start();
+    });
+}
+
+Object.keys(config.sauceTasks).forEach(createSauceTask);
+
+gulp.task('karma:sauce', ['lint'], function (done) {
+    var taskNames = Object.keys(config.sauceTasks).map(function (name) {
+        return 'karma:sauce:' + name;
+    });
+    taskNames.push(done);
+    runSequence.apply(null, taskNames);
 });
