7d8c63c909e0f694dab10420ca767c5c54575a2b
tmorin <tcmorin@gmail.com>
2015-08-18 21:43:06 +0200

    fix test according to browsers

--- es6/test/helper.js
+++ es6/test/helper.js
@@ -1,28 +1,36 @@
-// Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; InfoPath.3)
-function isIe9() {
-    return navigator.userAgent.indexOf('MSIE 9.0') > -1;
+function isIe() {
+    return navigator.userAgent.indexOf('MSIE 9.0') > -1 ||
+        navigator.userAgent.indexOf('MSIE 10.0') > -1 ||
+        navigator.userAgent.indexOf('Trident/7.0') > -1;
 }
 
-// Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.34 (KHTML, like Gecko) PhantomJS/1.9.8 Safari/534.34
 function isPhantom() {
     return navigator.userAgent.indexOf('PhantomJS') > -1;
 }
 
-// Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0
 function isFirefox() {
     return navigator.userAgent.indexOf('Firefox') > -1;
 }
 
-// Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0
+function isSafari() {
+    return navigator.userAgent.indexOf('Macintosh') > -1 &&
+        (
+            navigator.userAgent.indexOf('Version/7.0.') > -1 ||
+            navigator.userAgent.indexOf('Version/8.0.') > -1
+        )
+
+}
+
 function isAndroid4() {
-    return navigator.userAgent.indexOf('Firefox') > -1;
+    return navigator.userAgent.indexOf('Android 4') > -1;
 }
 
 export function canClick() {
     try {
         console.log(navigator.userAgent);
-    } catch(e) {}
-    return !(isIe9() || isPhantom() || isFirefox());
+    } catch (e) {
+    }
+    return !(isIe() || isPhantom() || isFirefox() || isSafari() || isAndroid4());
 }
 
 export function click(el) {
