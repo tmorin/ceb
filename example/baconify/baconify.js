bb45afa8825ca827c27266cf50e9dfbe4b9c6a41
tmorin <tcmorin@gmail.com>
2015-09-04 20:30:04 +0200

    review baconify example

--- example/baconify/baconify.js
+++ example/baconify/baconify.js
@@ -1,7 +1,36 @@
-import {Builder} from 'es6/lib/ceb.js';
+import {Builder, property} from 'es6/lib/ceb.js';
 import {Bus} from 'bacon';
 
 let counter = 0;
+
+function trigger(el, options, detail) {
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(options.name, options.bubbles, options.cancellable, detail);
+    return el.dispatchEvent(evt);
+}
+
+function createStream(el, id, apply, exposedName) {
+    destroyStream(el, id, exposedName);
+    let stream;
+    stream = el[id] = new Bus();
+    if (apply) {
+        stream = apply.call(el, el, el[id]) || stream;
+    }
+    if (exposedName) {
+        el.streams[exposedName] = stream;
+    }
+    return stream;
+}
+
+function destroyStream(el, id, exposedName) {
+    if (el.streams && el.streams[exposedName]) {
+        delete el.streams[exposedName];
+    }
+    if (el[id]) {
+        el[id].unsubAll();
+        el[id] = null;
+    }
+}
 
 export class BaconBuilder extends Builder {
 
@@ -15,22 +44,42 @@ export class BaconBuilder extends Builder {
         return this;
     }
 
+    trigger(name, bubbles = true, cancellable = true) {
+        this.data.dispachedEvent = {name, bubbles, cancellable};
+        return this;
+    }
+
     build(proto, on) {
-        let id = `_cebBaconifyBus-${counter++}}`,
-            apply = this.data.apply;
+
+        let id = `_cebBaconifyBus-${counter++}`,
+            apply = this.data.apply,
+            exposedName = this.data.exposedName,
+            dispachedEvent = this.data.dispachedEvent;
+
+        if (exposedName && !Object.getOwnPropertyDescriptor(proto, 'streams')) {
+            property('streams').immutable().value({}).build(proto, on);
+        }
+
+        on('before:createdCallback').invoke(el => {
+            createStream(el, id, apply, exposedName);
+        });
 
         on('before:attachedCallback').invoke(el => {
-            el[id] = new Bus();
-            apply.call(el, el, el[id]);
+            let stream = createStream(el, id, apply, exposedName);
+            if (dispachedEvent) {
+                stream.onValue(value => trigger(el, dispachedEvent, value));
+            }
         });
 
         on('before:detachedCallback').invoke(el => {
-            el[id].dispose();
+            destroyStream(el, id, exposedName);
         });
 
-        this.data.builder.invoke((el, evt) => {
-            el[id].push(evt);
-        }).build(proto, on);
+        if (this.data.builder) {
+            this.data.builder.invoke((el, evt) => {
+                el[id].push(evt);
+            }).build(proto, on);
+        }
     }
 }
 
--- example/baconify/demo.js
+++ example/baconify/demo.js
@@ -1,20 +1,155 @@
-import {ceb, template, on} from 'es6/lib/ceb.js';
+import $ from 'jquery';
+import {ceb, method, template, attribute, delegate, on} from 'es6/lib/ceb.js';
 import {baconify} from './baconify.js';
+import {fromPromise, once} from 'bacon';
 
-ceb().augment(
+$.support.cors = true;
+
+/**
+ * Get places from a query.
+ * If the query is falsy, the stream's value will be <code>null</code>.
+ * @param {string} [query] the query
+ * @returns {Bacon.EventStream} the stream of places
+ */
+function getPlaces(query) {
+    return !query ? once(null) : fromPromise($.ajax('http://nominatim.openstreetmap.org/search', {
+        crossDomain: true,
+        dataType: 'json',
+        method: 'GET',
+        data: {
+            format: 'json',
+            limit: '10',
+            q: query
+        }
+    }));
+}
+
+/**
+ * Get the keyCode from an event.
+ * @param {!Event} evt the event
+ * @return {number} the key code value
+ */
+function getKeyCode(evt) {
+    return (window.event ? window.event : evt).keyCode;
+}
+
+/**
+ * The <code>CebAddressSelector</code> element.
+ */
+export default ceb().augment(
     template(`
-        <input class="search">
+        <input type="text" placeholder="a place">
+        <input type="button" value="X">
         <ul></ul>
     `),
-    baconify(on('keydown').stop().delegate('input.search')).apply((el, stream) => {
-        return stream
-            .debounce(300)
-            .map(evt => evt.target.value)
-            .skipDuplicates()
-            .onValue(value => {
-                let li = document.createElement('li');
-                li.textContent = value;
-                el.querySelector('ul').appendChild(li);
+
+    /* Form elements' facade */
+
+    delegate(method('focus')).to('input[type=text]'),
+
+    delegate(attribute('value')).to('input[type=text]').property(),
+
+    delegate(attribute('name')).to('input[type=text]'),
+
+    delegate(attribute('disabled').setter((el, value) => {
+        el.querySelector('input[type=button]').disabled = value;
+        return value;
+    })).to('input[type=text]').property(),
+
+    delegate(attribute('readonly').setter((el, value) => {
+        el.querySelector('input[type=button]').disabled = value;
+        return value;
+    })).to('input[type=text]').property(),
+
+    /* Autocomplete's logic */
+
+    /* When the clear button is clicked, the value should be empty */
+    baconify(on('click').delegate('input[type=button]'))
+        .apply((el, stream) => stream.map(() => ''))
+        .trigger('value'),
+
+    /* When the input get new input, suggestions should be fetched */
+    baconify(on('input').delegate('input[type=text]'))
+        .apply((el, stream) => {
+            let query = stream.debounce(300).map(evt => evt.target.value);
+            let suggestions = query.flatMapLatest(getPlaces);
+            query.awaiting(suggestions)
+                .filter(value => value).onValue(() => el.querySelector('ul').innerHTML = 'Searching ...');
+            return suggestions;
+        })
+        .trigger('suggestions'),
+
+    /* When suggestions are fetched or overridden, they should be displayed */
+    baconify(on('suggestions'))
+        .apply((el, stream) => {
+            stream
+                .map(evt => evt.detail)
+                .onValue(results => {
+                    let ul = el.querySelector('ul');
+                    if (!results) {
+                        ul.innerHTML = '';
+                    } else if (results.length > 0) {
+                        ul.innerHTML = results
+                            .map(result => `<li value="${result.display_name}">${result.display_name}</li>`)
+                            .join('');
+                    } else {
+                        ul.innerHTML = 'No results found ...';
+                    }
+                });
+        }),
+
+    /* When up and down arrows are pressed, the marked suggestion should be updated */
+    baconify(on('keydown'))
+        .apply((el, stream) => {
+            let actionsMapping = {
+                38: 'previous',
+                40: 'next'
+            };
+            stream
+                .filter(evt => actionsMapping.hasOwnProperty(getKeyCode(evt)))
+                .doAction('.preventDefault')
+                .map(evt => actionsMapping[getKeyCode(evt)])
+                .map(action => {
+                    let selected = el.querySelector('ul li[marked]'),
+                        lis = el.querySelectorAll('ul li');
+                    if (selected) {
+                        selected.removeAttribute('marked');
+                    }
+                    if (lis.length > 0) {
+                        if (action === 'previous') {
+                            return selected && selected.previousSibling ? selected.previousSibling : lis.item(lis.length - 1);
+                        }
+                        return selected && selected.nextSibling ? selected.nextSibling : lis.item(0);
+                    }
+                })
+                .filter(li => li)
+                .onValue(li => li.setAttribute('marked', ''));
+        }),
+
+    /* When enter is pressed, the marked suggestion should be the value */
+    baconify(on('keydown'))
+        .apply((el, stream) => {
+            return stream
+                .filter(evt => getKeyCode(evt) === 13 && el.querySelector('ul li[marked]'))
+                .doAction('.preventDefault')
+                .map(() => el.querySelector('ul li[marked]'))
+                .map(li => li.getAttribute('value'));
+        })
+        .trigger('value'),
+
+    /* When a suggestion is selected, the suggestion should be the value */
+    baconify(on('click').delegate('ul li').skip())
+        .apply((el, stream) => stream.map(evt => evt.target.getAttribute('value')))
+        .trigger('value'),
+
+    /* When the value is updated, the suggestion should be cleared and the input focused */
+    baconify(on('value'))
+        .apply((el, stream) => {
+            stream.map(evt => evt.detail).onValue(value => {
+                el.value = value;
+                el.focus();
             });
-    })
-).register('ceb-input');
+            return stream.map(() => null);
+        })
+        .trigger('suggestions')
+).register('ceb-address-selector');
