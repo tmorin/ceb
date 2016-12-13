af805fcb795ea743f9d9f3461ff4fad99dbe7062
tmorin <thibault@morin.io>
2016-12-13 22:35:18 +0100

    fix dist files and example

--- test/ceb.on.spec.js
+++ test/ceb.on.spec.js
@@ -55,7 +55,7 @@ describe('ceb.on()', function () {
         });
     });
 
-    context.only('listen keyboard events', () => {
+    context('listen keyboard events', () => {
         let bubblingListener, captureListener, el;
         beforeEach(done => {
             console.log(typeof KeyboardEvent, navigator.appName, navigator.userAgent);
