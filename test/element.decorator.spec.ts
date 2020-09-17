0ffcb7638c2061709c040436bb5bd3aa4b1d7a86
tmorin <thibault@morin.io>
2020-09-17 09:13:28 +0200

    chore: add more tests about customized built-in elements

--- test/element.decorator.spec.ts
+++ test/element.decorator.spec.ts
@@ -14,10 +14,43 @@ describe('element.decorator', () => {
         })
         class TestElement extends HTMLElement {
             altName = 'a name';
+
+            connectedCallback() {
+                this.setAttribute('att0', 'val0');
+            }
         }
 
         const el: TestElement = sandbox.appendChild(document.createElement(tagName));
         assert.ok(sandbox.querySelector(tagName));
-        assert.strictEqual(el.altName, "a name");
+        assert.strictEqual(el.altName, 'a name');
+        assert.strictEqual(el.getAttribute('att0'), 'val0');
+    });
+    it('should create a customized built-in element with decorator', () => {
+        const tagName = 'input-test';
+
+        @ElementBuilder.element<InputTestElement>({
+            name: tagName,
+            extends: 'input'
+        })
+        class InputTestElement extends HTMLInputElement {
+            altName = 'a name';
+
+            constructor() {
+                super();
+            }
+
+            connectedCallback() {
+                this.setAttribute('att0', 'val0');
+            }
+        }
+
+        const el: InputTestElement = sandbox.appendChild(document.createElement('input', {is: tagName}));
+        {
+            const input = sandbox.querySelector('input');
+            assert.ok(input instanceof InputTestElement);
+            assert.strictEqual(input.outerHTML, `<input is="${tagName}" att0="val0">`);
+        }
+        assert.strictEqual(el.altName, 'a name');
+        assert.strictEqual(el.getAttribute('att0'), 'val0');
     });
 });
