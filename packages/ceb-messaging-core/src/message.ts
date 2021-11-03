bf6cda3c875d86c0c7871f040b681d0a719e271a
tmorin <thibault@morin.io>
2021-11-03 11:08:34 +0100

    feat(ceb-messaging-core): refactor the interface `Message` to embrace messaging guidelines
    BREAKING CHANGE: the field `Message.messageId` has been moved to `Message.headers.messageId`

--- packages/ceb-messaging-core/src/message.ts
+++ packages/ceb-messaging-core/src/message.ts
@@ -4,6 +4,34 @@
 export type MessageId = string
 
 /**
+ * The identifier of a message.
+ */
+export type MessageType = string
+
+/**
+ * The kind of a message.
+ */
+export enum MessageKind {
+    message = "message",
+    action = "action",
+    command = "command",
+    query = "query",
+    result = "result",
+    error = "error",
+    event = "event",
+}
+
+/**
+ * The name of a message.
+ */
+export interface MessageHeaders {
+    messageType: MessageType
+    messageId: MessageId
+
+    [key: string]: string | number
+}
+
+/**
  * The constructor of a message.
  * @template M the type of the message
  */
@@ -14,9 +42,11 @@ export interface MessageConstructor<M extends Message = Message> {
 /**
  * A message is an information which can be identified.
  * @template B the type of the body
+ * @template H the type of the headers
  */
-export interface Message<B = any> {
-    readonly messageId: MessageId
+export interface Message<B = any, H extends MessageHeaders = MessageHeaders> {
+    readonly kind: MessageKind
+    readonly headers: H
     readonly body: B
 }
 
@@ -24,71 +54,47 @@ export interface Message<B = any> {
  * An action is a message which expects a result.
  * It can be to mutate the system, c.f. {@link MessageCommand} or request a view of the system state, c.f. {@link MessageQuery}.
  * @template B the type of the body
+ * @template H the type of the headers
  */
-export interface MessageAction<B = any> extends Message<B> {
+export interface MessageAction<B = any, H extends MessageHeaders = MessageHeaders> extends Message<B, H> {
 }
-
-/**
- * The type of an action is its class or a string
- */
-export type MessageActionType<M extends MessageAction = MessageAction> = MessageConstructor<M> | string
 
 /**
  * A command is a message which triggers an action which can leads to side effects on the system state.
  * @template B the type of the body
+ * @template H the type of the headers
  */
-export interface MessageCommand<B = any> extends MessageAction<B> {
+export interface MessageCommand<B = any, H extends MessageHeaders = MessageHeaders> extends MessageAction<B, H> {
 }
-
-/**
- * The type of a command is the class or its related Custom-Event's type
- */
-export type MessageCommandType<M extends MessageCommand = MessageCommand> = MessageConstructor<M> | string
 
 /**
  * A query is a message which requests a view of the system state.
  * @template B the type of the body
+ * @template H the type of the headers
  */
-export interface MessageQuery<B = any> extends MessageAction<B> {
+export interface MessageQuery<B = any, H extends MessageHeaders = MessageHeaders> extends MessageAction<B, H> {
 }
-
-/**
- * The type of a query is the class or its related Custom-Event's type
- */
-export type MessageQueryType<M extends MessageQuery = MessageQuery> = MessageConstructor<M> | string
 
 /**
  * A result is the message providing insight about the execution of a command or a query.
  * @template B the type of the body
+ * @template H the type of the headers
  */
-export interface MessageResult<B = any> extends Message<B> {
+export interface MessageResult<B = any, H extends MessageHeaders = MessageHeaders> extends Message<B, H> {
 }
-
-/**
- * The type of a result is the class or its related Custom-Event's type
- */
-export type MessageResultType<M extends MessageResult = MessageResult> = MessageConstructor<M> | string
 
 /**
  * An error is a result describing the failure of the execution.
  * @template B the type of the error
+ * @template H the type of the headers
  */
-export interface MessageError<B extends Error = Error> extends MessageResult<B> {
+export interface MessageError<B extends Error = Error, H extends MessageHeaders = MessageHeaders> extends MessageResult<B, H> {
 }
-
-/**
- * The type of an error is the class or its related Custom-Event's type
- */
-export type MessageErrorType<M extends MessageError = MessageError> = MessageConstructor<M> | string
 
 /**
  * An event is a message which describes something which happened in the past.
  * @template B the type of the body
+ * @template H the type of the headers
  */
-export interface MessageEvent<B = any> extends Message<B> {
+export interface MessageEvent<B = any, H extends MessageHeaders = MessageHeaders> extends Message<B, H> {
 }
-
-/**
- * The type of an event is its class or a string
- */
-export type MessageEventType<M extends MessageEvent = MessageEvent> = MessageConstructor<M> | string
