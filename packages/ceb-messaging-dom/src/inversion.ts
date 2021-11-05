dbae0e07675be5add53beeaf3b69a37c1803e07b
tmorin <thibault@morin.io>
2021-11-05 16:33:57 +0100

    feat(ceb-messaging-dom): add options to the inversion module

--- packages/ceb-messaging-dom/src/inversion.ts
+++ packages/ceb-messaging-dom/src/inversion.ts
@@ -1,6 +1,16 @@
-import {AbstractModule} from "@tmorin/ceb-inversion";
-import {BusSymbol} from "@tmorin/ceb-messaging-core";
+import {AbstractModule, ComponentSymbol} from "@tmorin/ceb-inversion";
+import {Bus, BusSymbol} from "@tmorin/ceb-messaging-core";
 import {DomBus} from "./bus";
+
+/**
+ * The options of {@link DomModule}.
+ */
+export interface DomModuleOptions {
+    /**
+     * When `true`, the `error` internal events (i.e. `bus.on("error", ...)`) are displayed using `console.error(...)`.
+     */
+    errorToConsole: boolean
+}
 
 /**
  * The module registers a {@link Bus} bound with the key {@link BusSymbol}
@@ -17,6 +27,9 @@ export class DomModule extends AbstractModule {
     constructor(
         private readonly global: EventTarget = window,
         private readonly requester: EventTarget = global,
+        private readonly options: DomModuleOptions = {
+            errorToConsole: false
+        },
         private readonly bus = new DomBus(global, requester)
     ) {
         super();
@@ -24,9 +37,16 @@ export class DomModule extends AbstractModule {
 
     async configure(): Promise<void> {
         this.registry.registerValue(BusSymbol, this.bus)
-    }
-
-    async dispose(): Promise<void> {
-        await this.bus.dispose()
+        this.registry.registerFactory(ComponentSymbol, (registry) => ({
+            configure: async () => {
+                const bus = registry.resolve<Bus>(BusSymbol)
+                if (this.options.errorToConsole) {
+                    bus.on("error", error => console.error("DomBus throws an error", error))
+                }
+            },
+            async dispose() {
+                await registry.resolve<Bus>(BusSymbol).dispose()
+            }
+        }))
     }
 }
